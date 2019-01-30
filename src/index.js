e47a30553e67e21b129abee927873560c2417b84
DanielCarlos <daniel.carlos.souza@gmail.com>
2019-01-30 16:39:07 -0200

    projeto

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Routes from './routes';
+import "./config/StatusBarConfig";
+
+const App = ()=> <Routes/>;
+// export default class App extends Component {
+//     render() {
+//       return (
+//         <Routes/>
+//       );
+//     }
+//   }
+
+
+
+// class App extends Component {
+//     render(){
+//         return <Routes/>;
+//     }
+// }
+
+export default App;
+
+
+
+
+
