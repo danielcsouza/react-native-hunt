ed4803126bb29610876e3d8782d1ddd85ed29a91
DanielCarlos <daniel.carlos.souza@gmail.com>
2019-02-03 12:46:24 -0200

    mudando api para consumir a minha api feita em nodejs no heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://rocketseat-node.herokuapp.com/api'
+    //baseURL: 'https://rocketseat-node.herokuapp.com/api'
+    baseURL: 'https://nodejs-daniel-api.herokuapp.com/api'
 });
 
 export default api;
